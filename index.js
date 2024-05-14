f7b9c1fc6d94614fbeb4828f515617af5bebd462
jngwk <jngwk.ch@gmail.com>
2024-05-14 19:03:39 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import OpenAI from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const openai = new OpenAI({
+  apiKey: "sk-proj-kateEYfl6IeMGMnXNXcsT3BlbkFJ4Kl6LDcyZaVVOtAjQdXr",
+});
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.get("/", async (req, res) => {
+  async function main() {
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "user",
+          content:
+            "너가 요리와 음악 전문가라고 상상하고 돈까스와 어울리는 음악 추천해줘 다음 상황도 고려해줘: 장르: 재즈 , 시간대: 저녁",
+        },
+      ],
+      model: "gpt-3.5-turbo",
+    });
+
+    // console.log(completion.choices[0].message);
+    res.json({
+      completion: completion.choices[0].message,
+    });
+  }
+
+  main();
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
